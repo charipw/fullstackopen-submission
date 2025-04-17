dc7cb2315f2b7b3065599aa61893b7f23a3bcf37
Charleen Ipping Wah <charleen.ipping@gmail.com>
2025-04-17 14:03:22 +0200

    exercise 1.11

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -19,12 +19,36 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
+      <tr>
+        <td>
+          <StatisticLine text="good" value={good} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <StatisticLine text="neutral" value={neutral} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <StatisticLine text="bad" value={bad} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <StatisticLine text="all" value={all} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <StatisticLine text="average" value={average} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <StatisticLine text="positive" value={positive} />
+        </td>
+      </tr>
     </div>
   );
 };
