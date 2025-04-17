ff9f782fd47592ce060b7a16610ec987e5a93f81
Charleen Ipping Wah <charleen.ipping@gmail.com>
2025-04-17 11:19:00 +0200

    exercise 1.7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -42,6 +42,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good + neutral + bad) / 3}</div>
+      <div>positive {good / (good + neutral + bad)}%</div>
     </div>
   );
 };
