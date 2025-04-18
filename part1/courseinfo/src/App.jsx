3980b3377cdfdd1dc19c16af347506d488fcd969
Charleen Ipping Wah <charleen.ipping@gmail.com>
2025-04-18 16:37:39 +0200

    Restoring courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,46 +1,60 @@
-import { useState } from "react";
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.number}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} number={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} number={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} number={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
+};
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
-
-  const handleVotes = () => {
-    const votesCopy = [...votes];
-    votesCopy[selected] += 1;
-    setVotes(votesCopy);
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
-
-  const handleClick = () => {
-    const randomAnecdote = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomAnecdote);
-  };
-
-  const maxVotes = Math.max(...votes);
-  const maxIndex = votes.indexOf(maxVotes);
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <div>{anecdotes[selected]}</div>
-      <p>has {votes[selected]} votes</p>
-      <div>
-        <button onClick={handleVotes}>vote</button>
-        <button onClick={handleClick}>next anecdote</button>
-      </div>
-      <h1>Anecdote with most votes</h1>
-      <div>{anecdotes[maxIndex]}</div>
-      <p>has {maxVotes} votes</p>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
