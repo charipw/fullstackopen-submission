7e295ebe3d224a8c9317e916816ae27dc3765b4c
Charleen Ipping Wah <charleen.ipping@gmail.com>
2025-05-07 16:24:13 +0200

    Refactor Part2 - step6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,20 @@
 import Course from "./components/Course";
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+const App = (props) => {
+  const { courses } = props;
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {courses.parts.map((course) => (
+          <p key={course.id}>
+            {course.name} {course.exercises}
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
